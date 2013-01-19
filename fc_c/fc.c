3b9a97d196c6c3e0b149468ad30e68fe7a4f2a72
Pcopfer <git@pcopfer.de>
2013-01-19 20:31:57 +0100

    aktivate gitignore, add stuff

---
+++ fc_c/fc.c
@@ -0,0 +1,9 @@
+//
+//  fc.c
+//  fc_c
+//
+//  Created by Christian Platz on 19.01.13.
+//  Copyright (c) 2013 C3MA. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ fc_c/fc.h
@@ -0,0 +1,14 @@
+//
+//  fc.h
+//  fc_c
+//
+//  Created by Christian Platz on 19.01.13.
+//  Copyright (c) 2013 C3MA. All rights reserved.
+//
+
+#ifndef fc_c_fc_h
+#define fc_c_fc_h
+
+
+
+#endif
