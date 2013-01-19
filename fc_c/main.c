625f85f3c30e54cbf1877ec051b5a9d3a07e3b6d
Pcopfer <git@pcopfer.de>
2013-01-19 16:41:03 +0100

    Initial Commit

---
+++ fc_c/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  fc_c
+//
+//  Created by Christian Platz on 19.01.13.
+//  Copyright (c) 2013 C3MA. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
