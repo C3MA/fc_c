274ca91a44245801b57ef285b4f77c276439d44b
Pcopfer <git@pcopfer.de>
2013-01-30 17:45:45 +0100

    Remove sensless header

--- fc_c/test.c
+++ fc_c/test.c
@@ -2,8 +2,6 @@
 //  test.c
 //  fc_c
 //
-//  Created by Christian Platz on 27.01.13.
-//  Copyright (c) 2013 C3MA. All rights reserved.
 //
 
 #include <stdio.h>
--- fc_c/test.h
+++ fc_c/test.h
@@ -2,8 +2,6 @@
 //  test.h
 //  fc_c
 //
-//  Created by Christian Platz on 27.01.13.
-//  Copyright (c) 2013 C3MA. All rights reserved.
 //
 
 #ifndef fc_c_test_h
