07c1101cc6425e2d74ee7c9e2bf2b8402f03a766
0110 <github@buxs.org>
2013-04-25 11:29:27 +0200

    Started a new binary, the server in order to test the client

---
+++ example/server.c
@@ -0,0 +1,19 @@
+/*
+ *  server.c
+ *  XCodeFC_C
+ *
+ *  Created by ollo on 25.04.13.
+ *  Copyright 2013 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <stdio.h>
+#include "fc.h"
+
+/*
+ * Small implementation of a server using the test module for the logic
+ */
+int main(int argc, char *argv[])
+{
+	printf("Starting a server\n");
+}
