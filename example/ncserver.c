55c4bd9cd2579194f4047380a8c9da39617e5cda
0110 <github.com@buxs.org>
2013-12-15 11:07:25 +0100

    Ported server code into the simulator

--- example/ncserver.c
+++ example/ncserver.c
@@ -7,18 +7,101 @@
  * 
  * Copyright 2013 C3MA. All rights reserved.
  */
+
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
 #include "fcserver.h"
 
-void printUsage()
+/******************************************************************************
+ * IMPLEMENTATION FOR THE NECESSARY CALLBACKS
+ ******************************************************************************/
+
+void onNewImage(uint8_t* rgb24Buffer, int width, int height)
 {
-	printf("Usage: <width> <height>");
+	int i;
+
+	//printf("%d x %d\n", width, height);
+	for (i=0; i < width * height; i++) {
+		printf("%02X%02X%02X|", rgb24Buffer[i * 3 + 0],
+			   rgb24Buffer[i * 3 + 1], rgb24Buffer[i * 3 + 2]);
+
+		if ( (i + 1) % width == 0)
+		{
+			printf("\n");
+		}
+	}
+	printf("------------------------------------------\n");
+}
+
+void onClientChange(uint8_t totalAmount, fclientstatus_t action, int clientsocket)
+{
+	printf("Callback client %d did %X\t[%d clients]\n", clientsocket, action, totalAmount);
+}
+
+/******************************************************************************
+ * LOCAL FUNCTIONS
+ ******************************************************************************/
+
+static fcserver_t		server;
+
+// Define the function to be called when ctrl-c (SIGINT) signal is sent to process
+void signal_callback_handler(int signum)
+{
+	printf("Caught signal %d\n",signum);
+
+
+	printf("Stopping the Server...\n");
+
+	/* clean everything */
+	fcserver_close(&server);
+
+	// Terminate program
+	exit(signum);
+}
+
+void printUsage(char *appl)
+{
+	printf("Usage: %s <width> <height>\n", appl);
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	int width;
+	int height;
+	fcserver_ret_t	ret;
+	int sleeptime;
+
+	if (argc < 3)
 	{
-		printUsage();
+		printUsage(argv[0]);
+		return 2;
 	}
+
+	width = atoi(argv[1]);
+	height = atoi(argv[2]);
+
+
+	ret = fcserver_init(&server, &onNewImage, &onClientChange, width, height);
+	if (ret != FCSERVER_RET_OK)
+	{
+		printf("Server initialization failed with returncode %d\n", ret);
+		return 1;
+	}
+
+	fcserver_setactive(&server, 1 /* TRUE */);
+
+	sleeptime = 10;
+	do {
+		ret = fcserver_process(&server, sleeptime);
+
+		usleep( sleeptime * 1000 );/* wait 10ms */
+	} while ( ret == FCSERVER_RET_OK);
+
+	/* clean everything */
+	fcserver_close(&server);
+
+	return 0;
 }
